c930d5b059397335068da52a6c9b923553722098
ricky <rickyxiangzhihai@gmail.com>
2018-03-12 00:21:58 -0700

    universal hashing added

--- RandomMatrixHash.cpp
+++ RandomMatrixHash.cpp
@@ -1,8 +1,53 @@
 #include "RandomMatrixHash.h"
+#include <limits>
+#include <stdlib.h>
+#include <iostream>
 
-RandomMatrixHash::RandomMatrixHash(unsigned int m) {
+using namespace std;
+//m shoud be 2^n 0 <= n
+RandomMatrixHash::RandomMatrixHash(unsigned int m) 
+{
+    row = 0;
+    //calculate how many bits need to represent m
+    while (m >>= 1)
+        row++;
+    
+    //this can only used in linux which RANDMAX is INT_MAX
+    srand(time(NULL));
+    HashMatrix = (int*)malloc(row * sizeof(int));
+    for (int i = 0; i < row; i++)
+        HashMatrix[i] = rand() * (rand() % 2 ? 1 : -1);
+
+}
+
+bool parity(unsigned int x){
+    unsigned int count = 0, i, b = 1;
+
+    for(i = 0; i < 32; i++)
+        if( x & (b << i) ) 
+            count++;
+    
+    if(count % 2)
+        return false;
+
+    return true;
 }
 
 // Use mod 2 matrix multiplication for your hash function.
 size_t RandomMatrixHash::Hash(int key) const {
+    int hash = 0;
+    for (int i = 0; i < row; i++)
+    {
+        hash <<= 1;
+        hash |= parity(key & HashMatrix[i]);
+    }
+    return hash;
+}
+
+
+int main()
+{
+    RandomMatrixHash r(256);
+    cout << endl << r.Hash(16) << endl;
+
 }
--- RandomMatrixHash.h
+++ RandomMatrixHash.h
@@ -1,12 +1,15 @@
 #ifndef _RANDOM_MATRIX_HASH_H_
 #define _RANDOM_MATRIX_HASH_H_
 
+#include <cstddef>
+
 class RandomMatrixHash {
  public:
   RandomMatrixHash(unsigned int m);
   size_t Hash(int key) const;
 
  private:
+    int row, *HashMatrix;
 }; // RandomMatrixHash
 
 #endif  // _RANDOM_MATRIX_HASH_H_
